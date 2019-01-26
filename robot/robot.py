8a19278982a3b3e7ecde6f75a94cc1dd34d9e730
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2019-01-26 11:36:14 -0700

    Cleaned inits in robot.py

--- robot/robot.py
+++ robot/robot.py
@@ -13,14 +13,6 @@ from team3200.commands.lights import Lights
 from team3200.commands.lights import GoodGood
 import team3200.subsystems.driveTrain
 
-#from team3200.subsystems import driveTrain
-
-#code to help run the robot
-
-#import sys       
-def exit(retval):
-    pass
-#    sys.exit(retval)
 
 class MyRobot(commandbased.CommandBasedRobot):
     
@@ -28,10 +20,10 @@ class MyRobot(commandbased.CommandBasedRobot):
         team3200.getRobot = lambda x=0:self
         self.map = team3200.robotMap.RobotMap()
         self.networkTableInit()
-        self.dtSub = team3200.subsystems.driveTrain.DriveTrainSub()
-        self.driveController = wpilib.XboxController(0)
-        self.createButtons()
+        self.driveInit()
+        self.controllerInit()
         
+    
     def networkTableInit(self):
         NetworkTables.initialize(server = 'roborio-3200-frc.local')
         
@@ -39,12 +31,15 @@ class MyRobot(commandbased.CommandBasedRobot):
         self.liveWindowTable.putNumber('Sensitivity', -1)
         
 
-    def createButtons(self):
+    def controllerInit(self):
+        self.driveController = wpilib.XboxController(0)
         self.lightButton = JoystickButton(self.driveController, 3)
         self.lightButton.whenPressed(Lights())
         self.goodGoodButton = JoystickButton(self.driveController, 6)
         self.goodGoodButton.whenPressed(GoodGood())
 
+    def driveInit(self):
+        self.dtSub = team3200.subsystems.driveTrain.DriveTrainSub()
 
 if __name__ == '__main__':
     try:
@@ -70,3 +65,6 @@ if __name__ == '__main__':
             print("Failed to patch runtime. Error", err)
     
     wpilib.run(MyRobot,physics_enabled=True)
+
+def exit(retval):
+    pass
