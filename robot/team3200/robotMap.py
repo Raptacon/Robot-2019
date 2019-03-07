5c2e07c7884a5eaa673094ae7699d0f324a5bdbb
DrKittyKat <43834685+DrKittyKat@users.noreply.github.com>
2019-03-06 19:32:48 -0700

    Update robotMap.py
    Changed comments and turned motors into self.motors

--- robot/team3200/robotMap.py
+++ robot/team3200/robotMap.py
@@ -1,11 +1,12 @@
+"""
+    Robot map gathers all the hard coded values needed to interface with 
+    hardware into a single location
+"""
 
 import wpilib.xboxcontroller
 import hal
+
 class RobotMap():
-    """
-    Robot map gathers all the hard coded values needed to interface with 
-    hardware into a single location
-    """
     def __init__(self):
         """intilize the robot map"""
         self.motorsMap = CANMap()
@@ -23,24 +24,25 @@ class CANMap():
         holds mappings to all the motors in the robot
         '''
         rampRate = .2
-        #rotRampRate = .2
         pid = None
-        lifterMotors = {}
-        driveMotors = {}
-        '''The code below is an example of code for the SparkMax motor controllers'''
-        #shooterMotors['RotMotor'] = {'channel':4, 'inverted':False, 'type':'SparkMax', 'pid':rotPid, 'motorType':MotorType.kBrushless}
+        self.lifterMotors = {}
+        self.driveMotors = {}
+        
+        '''The code below is an example of the hardcoded values for the SparkMax motor controllers'''
+        #motorList['ExampleMotor'] = {'channel':4, 'inverted':False, 'type':'SparkMax', 'pid':rotPid, 'motorType':MotorType.kBrushless}
+        '''The code below is an example of the hardcoded values for the CANTalon motor controllers'''
+        #motorList['ExampleMotor'] = {'channel':4, 'inverted':True, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
+        
         '''The code below is for controlling TalonSRX motor controllers as well as their followers'''
-        driveMotors['leftMotor'] = {'channel':0, 'inverted':False, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
-        driveMotors['leftFollower'] = {'channel':3, 'inverted':False, 'type':'CANTalonFollower', 'masterChannel':0, "rampRate":rampRate}
-        driveMotors['rightMotor'] = {'channel':1, 'inverted':False, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
-        driveMotors['rightFollower'] = {'channel':2, 'inverted':False, 'type':'CANTalonFollower', 'masterChannel':1, "rampRate":rampRate}
+        self.driveMotors['leftMotor'] = {'channel':0, 'inverted':False, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
+        self.driveMotors['leftFollower'] = {'channel':3, 'inverted':False, 'type':'CANTalonFollower', 'masterChannel':0, "rampRate":rampRate}
+        self.driveMotors['rightMotor'] = {'channel':1, 'inverted':False, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
+        self.driveMotors['rightFollower'] = {'channel':2, 'inverted':False, 'type':'CANTalonFollower', 'masterChannel':1, "rampRate":rampRate}
         
-        lifterMotors['liftMotor'] = {'channel':4, 'inverted':True, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
-        lifterMotors['liftMotor2'] = {'channel':5, 'inverted':False, 'type':'CANTalonFollower', 'masterChannel':4, "rampRate":rampRate}
-        lifterMotors['roller'] = {'channel':6, 'inverted':False, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
+        self.lifterMotors['liftMotor'] = {'channel':4, 'inverted':True, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
+        self.lifterMotors['liftMotor2'] = {'channel':5, 'inverted':False, 'type':'CANTalonFollower', 'masterChannel':4, "rampRate":rampRate}
+        self.lifterMotors['roller'] = {'channel':6, 'inverted':False, 'type':'CANTalon', 'pid':pid, "rampRate":rampRate}
         
-        self.driveMotors = driveMotors
-        self.lifterMotors = lifterMotors
 
 class PneumaticsMap():
     def __init__(self):
