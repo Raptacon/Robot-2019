a01991e05a41ea99af6e736315628105d77dea5c
Team3200 <46548262+Team3200@users.noreply.github.com>
2020-01-13 19:14:38 -0800

    Better velocity (WORKING)
    Pid could be tuned better, but it will need to be tuned anyway for any load connected to the motor. This is a working state.

--- robot/team3200/robotMap.py
+++ robot/team3200/robotMap.py
@@ -30,7 +30,7 @@ class CANMap():
         rampRate = .2
         #rotRampRate = .2
         pid = {'controlType':ctre.WPI_TalonSRX.ControlMode.Velocity, 'feedbackDevice':0, 'sensorPhase':False, 'kPreScale': 7000, 'kP': .125, 'kI': 0, 'kD': .3, 'kF': 0} #These are all values to be tuned
-        pidSpark = {'controlType':rev.ControlType.kVelocity, 'feedbackDevice':0, 'sensorPhase':False, 'kPreScale': 1000, 'kP': 0.0005, 'kI': 0, 'kD': 0.05, 'kF': 0} #These are all values to be tuned (Prescale is a multiplier, position is measured in 1/6 rotations.)
+        pidSpark = {'controlType':rev.ControlType.kVelocity, 'feedbackDevice':0, 'sensorPhase':False, 'kPreScale': 1000, 'kP': 0.0004, 'kI': 0, 'kD': 3, 'kF': 0.3} #These are all values to be tuned (Prescale is a multiplier, position is measured in 1/6 rotations.)
         lifterMotors = {}
         driveMotors = {}
         '''The code below is an example of code for the SparkMax motor controllers'''
